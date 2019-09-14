6bef8f247264801e09c7a3b893e4269d3e6a7cb9
anibanchristianyouthsociety <55310214+anibanchristianyouthsociety@users.noreply.github.com>
2019-09-14 14:55:40 +0800

    init

---
+++ js/chunk-2d0d7286.9eda8a39.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7286"],{"762c":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("This is a report page")])},r=[],s=t("2877"),u={},a=Object(s["a"])(u,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0d7286.9eda8a39.js.map
